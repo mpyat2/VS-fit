3c7e713e44d6b9bf03e06ceafd35486bd09da05e
mpyat2 <mpyat2@gmail.com>
2025-11-19 15:08:11 +0200

    N of independent periods increased to 9

--- code/fitParamDialog.py
+++ code/fitParamDialog.py
@@ -35,16 +35,16 @@ def paramCheck(dialog,
     n_of_periods = len(strVarPeriods)
     
     try:
-        aD = int(eval(strVarAlgDeg.get(), {}, {}))
+        aD = int(eval(strVarAlgDeg.get() or "0", {}, {}))
         if aD < 0:
             raise Exception('Error', 'Algebraic polynomial degree must be >= 0')
         param_algDegree = aD
             
         for i in range(n_of_periods):
-            tD = int(eval(strVarDegrees[i].get(), {}, {}))
+            tD = int(eval(strVarDegrees[i].get() or "0", {}, {}))
             if tD < 0 or tD > 9:
                 raise Exception('Error', f'Trigonometric polynomial {i+1} degree must be between 0 and 9')
-            tP = float(eval(strVarPeriods[i].get(), {}, {}))
+            tP = float(eval(strVarPeriods[i].get() or "0", {}, {}))
             if tP < 0:
                 raise Exception('Error', f'Trigonometric polynomial {i+1} period must be >= 0')
             tO = optFlags[i].get()
@@ -75,7 +75,7 @@ def createCheckBox(frame, intvariable, initValue, r1, c1):
     return checkbox
 
 def fitParameters(master):
-    MAX_PERIODS = 5
+    MAX_PERIODS = 9
     
     global param_defined    
     global param_algDegree
@@ -92,7 +92,7 @@ def fitParameters(master):
     paramDialog = Toplevel(master)
     paramDialog.protocol("WM_DELETE_WINDOW", lambda: paramDialogDestroy(paramDialog))
     paramDialog.title("Parameters")
-    paramDialog.geometry("800x240+" + str(x+20) + "+" + str(y+20))
+    paramDialog.geometry("800x360+" + str(x+20) + "+" + str(y+20))
 
     frame = Frame(paramDialog)
     frame.pack(pady=10)
@@ -133,9 +133,9 @@ def fitParameters(master):
                                                  strVarDegrees,
                                                  intVarOptFlags,
                                                  intVarBootstrapErr))
-    buttonOK.grid(row=7, column=1)
+    buttonOK.grid(row=MAX_PERIODS + 2, column=1)
     buttonCancel = Button(frame, text="Cancel", command=lambda: paramDialogDestroy(paramDialog))
-    buttonCancel.grid(row=7, column=4)
+    buttonCancel.grid(row=MAX_PERIODS + 2, column=4)
     paramDialog.transient(master)
     paramDialog.grab_set()
     paramDialog.focus_set()
